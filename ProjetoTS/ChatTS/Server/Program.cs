3b89d0d30472659c35eba01a8dd8f05261ebc03e
Igor Guilherme Oleiro Patrocinio <2241551@my.ipleiria.pt>
2025-04-14 22:00:14 +0100

    BD Created
    No servidor, já estão criadas as classes, e o context... A Base de Dados já foi gerada...

---
+++ ProjetoTS/ChatTS/Server/Program.cs
@@ -0,0 +1,29 @@
+﻿using Server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+
+            using (var db = new ChatContext())
+            {
+                var User = new User { Name = "Igor",  Username= "asd", Password="123" };
+                db.Users.Add(User);
+
+          
+                db.SaveChanges();
+            }
+
+        }
+    }
+}
---
+++ ProjetoTS/ChatTS/Server/models/UserRoom.cs
@@ -0,0 +1,35 @@
+﻿using Server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.models
+{
+    class UserRoom
+    {
+        public UserRoom()
+        {
+        }
+
+        public int Id { get; set; }
+        public int IdUser { get; set; }
+        public User User { get; set; }
+        public int IdRoom { get; set; }
+        public Room Room { get; set; }
+
+        public string UserType { get; set; } // se vai ser Guest ou Admin
+        public DateTime DateCreated { get; set; }
+        public string UserState { get; set; } // vamos guardar esta variavel, para se quisermos excluir um membro de uma sala por exemplo
+
+        public UserRoom(int idUser, int idRoom, string userType, string userState)
+        {
+            IdUser = idUser;
+            IdRoom = idRoom;
+            UserType = userType;
+            DateCreated = DateTime.Now;
+            UserState = userState;
+        }
+    }
+}
