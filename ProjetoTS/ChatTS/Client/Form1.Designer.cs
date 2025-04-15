05ffb2286ec23625beb5c551281b836267bfbeac
Igor Guilherme Oleiro Patrocinio <2241551@my.ipleiria.pt>
2025-04-15 22:31:17 +0100

    ficha3 integrada na base
    ficha 3 das aulas integrada na base

--- ProjetoTS/ChatTS/Client/Form1.Designer.cs
+++ ProjetoTS/ChatTS/Client/Form1.Designer.cs
@@ -28,13 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonQuit = new System.Windows.Forms.Button();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            this.labelMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonQuit
+            // 
+            this.buttonQuit.Location = new System.Drawing.Point(351, 163);
+            this.buttonQuit.Name = "buttonQuit";
+            this.buttonQuit.Size = new System.Drawing.Size(75, 23);
+            this.buttonQuit.TabIndex = 7;
+            this.buttonQuit.Text = "Sair";
+            this.buttonQuit.UseVisualStyleBackColor = true;
+            this.buttonQuit.Click += new System.EventHandler(this.buttonQuit_Click);
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(351, 124);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 23);
+            this.buttonSend.TabIndex = 6;
+            this.buttonSend.Text = "Enviar";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.Location = new System.Drawing.Point(24, 124);
+            this.textBoxMessage.Multiline = true;
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.Size = new System.Drawing.Size(287, 62);
+            this.textBoxMessage.TabIndex = 5;
+            // 
+            // labelMessage
+            // 
+            this.labelMessage.AutoSize = true;
+            this.labelMessage.Location = new System.Drawing.Point(21, 108);
+            this.labelMessage.Name = "labelMessage";
+            this.labelMessage.Size = new System.Drawing.Size(151, 13);
+            this.labelMessage.TabIndex = 4;
+            this.labelMessage.Text = "Texto a enviar para o servidor:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonQuit);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBoxMessage);
+            this.Controls.Add(this.labelMessage);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonQuit;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.TextBox textBoxMessage;
+        private System.Windows.Forms.Label labelMessage;
     }
 }
 
--- ProjetoTS/ChatTS/Client/Form1.cs
+++ ProjetoTS/ChatTS/Client/Form1.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using EI.SI;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Sockets;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +15,72 @@ namespace Client
 {
     public partial class Form1: Form
     {
+        private const int PORT = 10000;
+        NetworkStream networkStream;
+        ProtocolSI protocolSI;
+        TcpClient client;
+
         public Form1()
         {
             InitializeComponent();
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Loopback, PORT);
+            client = new TcpClient();
+            client.Connect(endpoint);
+            networkStream = client.GetStream();
+            protocolSI = new ProtocolSI();
         }
+
+        // Método do botão enviar
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            string msg = textBoxMessage.Text;
+            textBoxMessage.Clear();
+            byte[] packet = protocolSI.Make(ProtocolSICmdType.DATA, msg); //cria uma mensagem/pacote de um tipo específico
+            networkStream.Write(packet, 0, packet.Length);
+            while (protocolSI.GetCmdType() != ProtocolSICmdType.ACK)
+            {
+                networkStream.Read(protocolSI.Buffer, 0, protocolSI.Buffer.Length);
+            }
+        }
+        // O envio de bits/bytes é trivial recorrendo ao ProtocolSI.
+
+        //Método para fechar o Client
+        private void CloseClient()
+            {
+                try
+                {
+                    // Definição da variável eot (End of Transmission) do tipo array de byte.
+                    // Utilização do método Make. ProtocolSICmdType serve para enviar dados
+                    byte[] eot = protocolSI.Make(ProtocolSICmdType.EOT);
+
+                    // A classe NetworkStream disponibiliza métodos para enviar/receber dados através de socket Stream
+                    // O Socket de rede é um endpoint interno para envio e recepção de dados com um nó/computador presente na rede.
+                    networkStream.Write(eot, 0, eot.Length);
+                    networkStream.Read(protocolSI.Buffer, 0, protocolSI.Buffer.Length);
+                    networkStream.Close();
+                    client.Close();
+                }
+
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            //Método para fechar o formulário
+            private void Client_FormClosing(object sender, FormClosingEventArgs e)
+            {
+                // Chamar a função para fechar o Client
+                CloseClient();
+            }
+
+            //Método para o Botão para sair
+            private void buttonQuit_Click(object sender, EventArgs e)
+            {
+                // Chamar a função para fechar o Client e associar a este próprio botão
+                CloseClient();
+                this.Close();
+            }
+
     }
 }
